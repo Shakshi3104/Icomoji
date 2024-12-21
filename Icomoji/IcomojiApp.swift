eee2456660dc00ab602e9dfa82df1b32cfbd4a64
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2024-12-21 13:45:51 +0900

    Initial Commit

---
+++ Icomoji/IcomojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcomojiApp.swift
+//  Icomoji
+//
+//  Created by MacBook Pro M1 on 2024/12/21.
+//
+
+import SwiftUI
+
+@main
+struct IcomojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
